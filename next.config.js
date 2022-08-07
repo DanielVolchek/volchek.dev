1debed41a28c7cc6f1b1aea2781c739598c1f552
Daniel Volchek <danielvolchek123@gmail.com>
2022-08-06 21:36:05 -0400

    moved redirect to next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/github',
+        destination: 'https://github.com/danielvolchek',
+        permanent: true
+      },
+      {
+        source: '/linkedin',
+        destination: 'https://www.linkedin.com/in/daniel-volchek-aa0172224/',
+        permanent: true
+      },
+    ]
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
