0ee92fe94469e2ca2dc8099b947c0f855c164ae8
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-23 16:32:05 -0500

    fixed bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ const Home: NextPage = () => {
   return <></>;
 };
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
   return {
     redirect: {
       destination: "https://github.com/danielvolchek",
